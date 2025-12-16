aef2efe83c7ead8e79ce59af4e0a102f4115e316
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-12-16 17:31:31 +0330

    create : server

---
+++ internal/server/http.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"gateway/internal/config"
+	"net/http"
+	"time"
+)
+
+type HttpServer struct {
+	server *http.Server
+}
+
+func New(cfg config.Config, handler http.Handler) *HttpServer {
+	addr := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
+
+	srv := &http.Server{
+		Addr:         addr,
+		Handler:      handler,
+		ReadTimeout:  time.Duration(cfg.Server.ReadTimeoutMs) * time.Millisecond,
+		WriteTimeout: time.Duration(cfg.Server.WriteTimeoutMs) * time.Millisecond,
+	}
+
+	return &HttpServer{
+		server: srv,
+	}
+}
+
+func (s *HttpServer) Start() error {
+	return s.server.ListenAndServe()
+}
+
+func (s *HttpServer) Shutdown(ctx context.Context) error {
+	return s.server.Shutdown(ctx)
+}
