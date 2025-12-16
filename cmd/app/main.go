c317adf30eedf349d0d88c427b2b34e83d2f20ac
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-12-16 15:13:44 +0330

    update : implement load config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"gateway/internal/config"
+	"log"
+)
+
+func main() {
+	cfg, err := config.Load("./configs/settings.yml")
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+		return
+	}
+
+	log.Printf("starting %s (%s) on %s:%d",
+		cfg.App.Name,
+		cfg.App.Env,
+		cfg.Server.Host,
+		cfg.Server.Port)
+
+	select {}
+}
