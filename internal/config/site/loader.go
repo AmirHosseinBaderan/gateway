7c89d01650d199236fc9d1fdee81caac09c574ba
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-12-16 18:39:41 +0330

    update : implement loader func

--- internal/config/site/loader.go
+++ internal/config/site/loader.go
@@ -1,1 +1,34 @@
 package site
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+func LoadSites(path string) ([]Config, error) {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var configs []Config
+	for _, file := range files {
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".yml") || file.Name() == "settings.yml" {
+			continue
+		}
+
+		data, err := os.ReadFile(filepath.Join(path, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		var config Config
+		if err := yaml.Unmarshal(data, &config); err != nil {
+			return nil, err
+		}
+		configs = append(configs, config)
+	}
+	return configs, nil
+}
